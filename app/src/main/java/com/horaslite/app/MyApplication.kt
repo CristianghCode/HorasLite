e6d06df64b1f13a414582a54e67f4beb4b30ea49
Cristian García Herrero <cristian.garcia@metaenlace.com>
2025-11-07 00:22:18 +0100

    feat: Initialize ThreeTenABP in Application class
    This commit adds the `MyApplication` class which extends `Application`. It initializes the ThreeTen Android Backport (ThreeTenABP) library in the `onCreate` method. This makes the modern date and time APIs available throughout the application.

---
+++ app/src/main/java/com/horaslite/app/MyApplication.kt
@@ -0,0 +1,11 @@
+// file: src/main/java/com/your/package/MyApplication.kt
+import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
+}
+    
